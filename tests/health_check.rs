c7a19c93e544d93fa9a48ba1df097c9855c2ce90
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-10-02 14:56:08 -0700

    finishes first pass impl at health_check

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,4 +4,19 @@ async fn health_check_works() {
     spawn_app().await.expect("Failed to spawn app");
     //brings in reqwest to perform HTTP requests against the app
     let client = reqwest::Client::new();
+
+    //Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    //Asserts
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> Result<(), std::io::Error> {
+    todo!()
 }
