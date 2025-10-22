3b9fb4f6c49d860eefee93967b0998d3034ec4f5
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-10-22 14:45:43 -0700

    adds a 400 data missing test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -47,3 +47,34 @@ async fn subscribe_return_a_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    //Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        //Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            //additional custom error message when test fails
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
