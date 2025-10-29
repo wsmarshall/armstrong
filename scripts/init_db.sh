fe72d89ca915a9d72ec8c9f0ee92c1cebb254586
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-10-29 14:45:59 -0700

    finishes first pass at Postgres init bash script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -14,3 +14,11 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 #launch Postgres using Docker
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
+  # ^ Increased max number of connections for testing 
