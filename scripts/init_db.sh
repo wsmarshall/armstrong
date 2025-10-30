237d825b2f95860c2c12ef023ad16c47599f55f5
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-10-30 14:57:40 -0700

    adds "up check" for postgres database ping to init script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -22,3 +22,16 @@ docker run \
   -d postgres \
   postgres -N 1000
   # ^ Increased max number of connections for testing 
+
+  #ping Postgres until it's up and ready to accept input/queries/commands
+  export PGPASSWORD="${DB_PASSWORD}"
+  until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -D "postgres" -c '\q'; do
+    >&2 echo "Postgres is currently unavailable - sleeping"
+    sleep 1
+  done
+
+  >&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+  DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+  export DATABASE_URL
+  sqlx database create
