a132f090afdd4af5fc7dfc1967de91965b8e0164
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-01 14:26:34 -0700

    starts debugging no driver for postgres URL scheme

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -43,12 +43,13 @@ fi
 
   #ping Postgres until it's up and ready to accept input/queries/commands
   export PGPASSWORD="${DB_PASSWORD}"
-  until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -D "postgres" -c '\q'; do
+  until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is currently unavailable - sleeping"
     sleep 1
   done
 
   >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
 
   DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
   export DATABASE_URL
