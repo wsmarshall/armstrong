9f9516f9bfb15a1b5d513727e20ab554c9fcf92c
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-01 13:36:46 -0700

    adds sqlx installed check to init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,7 +7,14 @@ if ! [ -x "$(command -v psql)" ]; then
     exit 1
 fi
 
-
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "    cargo install --version='~0.7' sqlx-cli \
+  --no-default-features --features rustls,postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
 
 #check if custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
