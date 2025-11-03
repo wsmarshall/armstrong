db7e1752f16522e3dd22ab9ac4d8ff89889c9fcb
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-03 13:43:46 -0800

    adds a first pass at get_configuration (for next-up yaml impl)

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,3 +12,16 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialise the configuration reader
+    let settings = config::Config::builder()
+        //add config values from file 'configuration.yaml'
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+    //try to convert config vals read into our Settings type
+    settings.try_deserialize::<Settings>()
+}
