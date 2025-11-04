f66c7f69df00a36b0c046773ec406e99af677756
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-04 13:56:20 -0800

    still bugfixing

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -51,8 +51,7 @@ fi
   >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 
-  DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
-  export DATABASE_URL
+  export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
   sqlx database create
   sqlx migrate run
 
