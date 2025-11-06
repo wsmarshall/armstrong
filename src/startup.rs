b8ec0b6a84f46910d4cd796148af96fab18d52c9
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-06 13:58:37 -0800

    mods startup.rs to fully use PgConnection

--- src/startup.rs
+++ src/startup.rs
@@ -4,10 +4,15 @@ use sqlx::PgConnection;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+    //wrap connection in smart pointer
+    let connection = web::Data::new(connection);
+    //capture 'connection' from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            //get a pointer copy and attach it with application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
