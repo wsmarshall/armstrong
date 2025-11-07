274e0c354d42cca1bb7a644fd5eaeab359c1962d
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-07 13:56:21 -0800

    mods subscribe fn to insert into database

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,5 +13,19 @@ pub async fn subscribe(
     _form: web::Form<FormData>,
     _connection: web::Data<PgConnection>,
 ) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    //use 'get_ref' to get immutable reference to the
+    //PgConnection which is wrapped by web::Data=
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
