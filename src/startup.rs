9be3744408f38cef40d03f0ab20795984ac71b9a
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-10 13:14:08 -0800

    mods startup with PgPool replacing PgConnection

--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,18 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{App, HttpServer, dev::Server, web};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    //wrap connection in smart pointer
-    let connection = web::Data::new(connection);
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    //wrap pool using web::Data, meaning an Arc smart pointer
+    let db_pool = web::Data::new(db_pool);
     //capture 'connection' from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             //get a pointer copy and attach it with application state
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
