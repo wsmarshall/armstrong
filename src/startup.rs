35b5735ec74297fa65225d0c633029cf1754c019
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-11 13:45:54 -0800

    adds Logger to startup sequence

--- src/startup.rs
+++ src/startup.rs
@@ -5,10 +5,12 @@ use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     //wrap pool using web::Data, meaning an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     //capture 'connection' from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            //middlewares use the 'wrap' method on 'App'
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             //get a pointer copy and attach it with application state
