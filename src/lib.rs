a6f9608cf799bcc74f393f64c9ff40957bffb1e1
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-11 15:35:51 -0800

    adds env to main and comments out Log trait in lib (unnecessary?)

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,24 @@ pub mod configuration;
 pub mod routes;
 pub mod startup;
 
-///a trait summing up operations required of a logger
-pub trait Log: Sync + Send {
-    ///Determines if a log message with the specified metadata
-    /// will be logged
-    ///
-    /// this is used by the 'log_enabled!' macro to allow
-    /// callers to avoid expensive computation of
-    /// log message arguments if the message
-    /// would be discarded anyway
-    fn enabled(&self, metadata: &Metadata) -> bool;
+// ///a trait summing up operations required of a logger
+// pub trait Log: Sync + Send {
+//     ///Determines if a log message with the specified metadata
+//     /// will be logged
+//     ///
+//     /// this is used by the 'log_enabled!' macro to allow
+//     /// callers to avoid expensive computation of
+//     /// log message arguments if the message
+//     /// would be discarded anyway
+//     fn enabled(&self, metadata: &Metadata) -> bool;
 
-    ///Logs the 'Record
-    ///
-    /// note that 'enabled' is NOT necessarily called before this
-    /// implementations of log should perform all required
-    /// filtering internally
-    fn log(&self, record: &Record);
+//     ///Logs the 'Record
+//     ///
+//     /// note that 'enabled' is NOT necessarily called before this
+//     /// implementations of log should perform all required
+//     /// filtering internally
+//     fn log(&self, record: &Record);
 
-    ///Flushes (clears) any buffered records
-    fn flush(&self);
-}
+//     ///Flushes (clears) any buffered records
+//     fn flush(&self);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use armstrong::configuration::get_configuration;
 use armstrong::startup::run;
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    //init calls set_logger, so all that is needed
+    //printing all logs at info-level or above by default
+    // if the RUST_LOG environment variable hasn't been set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     //Panic! if we can't get/read configuration
     let configuration = get_configuration().expect("Failed to get/read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
