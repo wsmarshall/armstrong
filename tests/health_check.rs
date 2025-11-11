4c86dc18ca5163d6ef8ca47545ef86a1bdc4cf27
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-11 13:27:51 -0800

    makes change to spawn app for database name indeterminacy enabling

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -11,6 +11,28 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    //create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    //migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
 //spin up the app, returns its address (e.g. http://localhost:XXXX)
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -19,9 +41,7 @@ async fn spawn_app() -> TestApp {
     let address = format!("http://127.0.0.1:{}", port);
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     TestApp {
