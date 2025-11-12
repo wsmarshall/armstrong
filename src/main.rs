4b9b9a5ffc2b9519f79b5c9c28ab2533ce741ab4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-12 15:06:05 -0800

    adds LogTracer to add back in actix-web logging capabilities

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    //redirects all 'log' events to our subscriber
+
+    LogTracer::init().expect("Failed to set logger");
     //printing all logs at info-level or above by default
     // if the RUST_LOG environment variable hasn't been set
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
