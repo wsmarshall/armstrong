98e059b914d5d2af39dc7d160783a557972a6a0f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-12 12:49:47 -0800

    removes unneeded log trait scource code (fr. external library)

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,3 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-// ///a trait summing up operations required of a logger
-// pub trait Log: Sync + Send {
-//     ///Determines if a log message with the specified metadata
-//     /// will be logged
-//     ///
-//     /// this is used by the 'log_enabled!' macro to allow
-//     /// callers to avoid expensive computation of
-//     /// log message arguments if the message
-//     /// would be discarded anyway
-//     fn enabled(&self, metadata: &Metadata) -> bool;
-
-//     ///Logs the 'Record
-//     ///
-//     /// note that 'enabled' is NOT necessarily called before this
-//     /// implementations of log should perform all required
-//     /// filtering internally
-//     fn log(&self, record: &Record);
-
-//     ///Flushes (clears) any buffered records
-//     fn flush(&self);
-// }
