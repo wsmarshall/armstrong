44f06775042481e74e260fc9ffce9a1e2f462234
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-12 12:48:17 -0800

    starts adding Span into subscriptions (better than log for scope)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,6 +12,11 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     //generate a random unique identifier
     let request_id = Uuid::new_v4();
+    //similar to logs, Spans have a "level"
+    //'info_span' creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name = %form.name);
+    let _request_span_guard = request_span.enter();
     tracing::info!(
         "request_id: {}; Adding '{}', '{}' as a new subscriber.",
         request_id,
