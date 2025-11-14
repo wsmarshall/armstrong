d734cdbdde437260af2a75238e37aac5a224a8de
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-14 11:22:04 -0800

    splits subscribe int db logic and routine calling

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -46,3 +46,29 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         }
     }
 }
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    //use 'get_ref' to get immutable reference to the
+    //PgConnection which is wrapped by web::Data=
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
