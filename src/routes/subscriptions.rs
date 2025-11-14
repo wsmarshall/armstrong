ee3c80dd0fbfce9d5f71bbcfd0e91bc48a3e0af4
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-14 11:14:26 -0800

    enables automatic span creation in subscribe fn

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,16 +21,6 @@ pub struct FormData {
 )]
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    //generate a random unique identifier
-    let request_id = Uuid::new_v4();
-    //similar to logs, Spans have a "level"
-    //'info_span' creates a span at the info-level
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name = %form.name);
-    let _request_span_guard = request_span.enter();
-
-    //no need to call 'enter' on query_span
-    //'.instrument' does that automatically in the future lifetime
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     match sqlx::query!(
