9f601105ee8f344c1db333c5bafe2695072d6c4f
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-21 13:47:50 -0800

    changes main: PgPool::connect to lazy connect

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ async fn main() -> Result<(), std::io::Error> {
 
     //Panic! if we can't get/read configuration
     let configuration = get_configuration().expect("Failed to get/read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres.");
     //NB: address now coming from our settings
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
