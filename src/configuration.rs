102b076a6bf4e0a3d564c21465b01a097c4dfb0d
Warren <42559252+wsmarshall@users.noreply.github.com>
2025-11-21 14:05:06 -0800

    finishes ApplicationSettings change propagation

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,10 @@ async fn main() -> Result<(), std::io::Error> {
         PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
     //NB: address now coming from our settings
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection)?.await
 }
